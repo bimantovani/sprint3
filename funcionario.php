925d15462e5f31658ea116dbd382ccfcda1c5b88
Bia Mantovani <beatrizmantovani28@gmail.com>
2024-11-01 16:54:58 -0300

    Update funcionario.php

--- funcionario.php
+++ funcionario.php
@@ -33,7 +33,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
     <h1>Login</h1>
